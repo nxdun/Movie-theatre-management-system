1c2ab9b0411ab8ab8f93bb272cb191a0e9429ac1
Vish0111 <it21315282@my.sliit.lk>
2023-08-26 17:37:28 +0530

    Server running on port 5000 sucsesfull

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+
+const app = express();
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
