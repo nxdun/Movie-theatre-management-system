5bb8387f6d38f8c15e86c846b7e94cd6a8fe76cd
Vish0111 <it21315282@my.sliit.lk>
2023-08-30 21:26:20 +0530

    Added Products Into the database

---
+++ backend/data/products.js
@@ -0,0 +1,43 @@
+const products = [
+    {
+      name: "Combo concession Pack 1",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYzOTjGhvPUIvzt_ItSRyjLmt_TarDy6W17w&usqp=CAU",
+      description:
+        "Special deal for movie lovers One Large Cheese Popcorn and One Medium Drink.",
+      price: 400,
+      countInStock: 15,
+    },
+    {
+      name: "Combo concession Pack 2",
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpUJojmjAeT1T0PczNPd-X0o1cbUg4RHImtQ&usqp=CAU",
+      description:
+        "Special deal for couples One Large Popcorn and Two Drinks (Medium).",
+      price: 900,
+      countInStock: 10,
+    },
+    {
+        name: "Movie Lover Merchandise Pack",
+        imageUrl:
+          "https://www.fanplus.com/products/842-848.jpg",
+        description:
+          "FAN+ T-Shirt + Cap For Rs.950.",
+        price: 950,
+        countInStock: 20,
+      },
+      {
+        name: "Fast X Merchandise Pack",
+        imageUrl:
+          "https://cdn.entertainment-focus.com/wp-content/uploads/2023/05/18224411/fastx_intl_16x9_packshot_publi-1-696x392.jpg",
+        description:
+          "Limited Offer For Fast X, Grab Your Fast X  -T-Shirt(XL,L,M,S Available)-Cap with Free Water Bottel For only Rs:1200.",
+        price: 1200,
+        countInStock: 10,
+      },
+    
+    
+  ];
+  
+  module.exports = products;
+  
