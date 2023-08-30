4e576ce00b0e60f416062bb321cb49d3c39f9b09
Vish0111 <it21315282@my.sliit.lk>
2023-08-30 21:36:00 +0530

    Backend Running Sucsessfully

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 
 const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
 
 connectDB();
 
 
 const app = express();
+app.use('/api/products', productRoutes);
 
 
 
