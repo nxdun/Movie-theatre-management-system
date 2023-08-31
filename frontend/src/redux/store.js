8894629a4d70af2876a4d3e6e8bd61892967e1c5
Vish0111 <it21315282@my.sliit.lk>
2023-09-01 00:34:21 +0530

    Modify Store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,16 +1,22 @@
 import {legacy_createStore as createStore , combineReducers, applyMiddleware} from 'redux';
+
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
+//Reducers
+import {cartReducer} from './reducers/cartReducers';
+import {getProductDetailsReducer, getProductsReducer} from './reducers/productReducers';
 
-
-
-const reduser = combineReducers({});
+const reducer = combineReducers({
+    cart: cartReducer,
+    getProducts: getProductsReducer,
+    getProductDetails: getProductDetailsReducer,
+});
 
 const middleware = [thunk];
 
 const store = createStore(
-    reduser,
+    reducer,
     composeWithDevTools(applyMiddleware(...middleware))
 );
 
