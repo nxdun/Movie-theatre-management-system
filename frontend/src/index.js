e243981d3bc6125ed68e4defe670bb3dc39df416
Vish0111 <it21315282@my.sliit.lk>
2023-08-31 23:58:25 +0530

    React-DOM  error fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>,
+  );
 
 reportWebVitals();
