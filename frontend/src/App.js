e64f1bc51ba460ea3b548c7544d805fe66550308
Vish0111 <it21315282@my.sliit.lk>
2023-08-31 10:23:59 +0530

    Screen files and Css files added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      
-    
-
-    </div>
+    <Router>
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+      <main className="app">
+        <Routes>
+          <Route exact path="/shop" element={<ShopScreen />} />
+          <Route exact path="/product/:id" element={<ProductScreen />} />
+          <Route exact path="/cart" element={<CartScreen />} />
+          
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ click }) => {
+  
+  
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <h2>GALAXY CINEMAS</h2>
+      </div>
+
+      <ul className="navbar__links">
+        <li>
+          <Link to="/cart" className="cart__link">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart <span className="cartlogo__badge"></span>
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/home">Shop</Link>
+        </li>
+      </ul>
+
+      <div className="hamburger__menu" onClick={click}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
