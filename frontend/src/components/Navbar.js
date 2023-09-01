46c4b03e551292348969ece30d070bbd483b11d5
Vish0111 <it21315282@my.sliit.lk>
2023-09-01 16:25:43 +0530

    Navbar Cart Count added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,16 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
 
 const Navbar = ({ click }) => {
+
+  const cart = useSelector ((state) => state.cart);
+  const {cartItems} = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => qty + Number(item.qty), 0);//each item qty added to cartCount and return
+  };
   
   
   return (
@@ -15,7 +24,7 @@ const Navbar = ({ click }) => {
           <Link to="/cart" className="cart__link">
             <i className="fas fa-shopping-cart"></i>
             <span>
-              Cart <span className="cartlogo__badge"></span>
+              Cart <span className="cartlogo__badge">{getCartCount()}</span>
             </span>
           </Link>
         </li>
