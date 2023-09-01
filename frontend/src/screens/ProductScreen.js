2c32b6ba34df4da6d717f2946debe9ac9d36138e
Vish0111 <it21315282@my.sliit.lk>
2023-09-01 13:25:56 +0530

    Render Each Product values dynamically in view

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,43 +1,80 @@
 import "./ProductScreen.css";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams} from 'react-router-dom'; // Import useParams and useNavigate
+import { getProductDetails } from '../redux/actions/productActions';
+
 
 const ProductScreen = () => {
-    return (
-        <div className="productscreen">
-            <div className="productscreen__left">
-                <div className="left__image">
-                    <img src="https://www.fanplus.com/products/842-848.jpg" alt="product name" />
-                    </div>
-                    <div className="left__info">
-                        <p className="left__name">Product 1</p>
-                        <p>Price: $499.99</p>
-                        <p>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                    </div>
-                </div>
-                <div className="productscreen__right">
-                    <div className="right__info">
-                        <p>
-                            Price: <span>$499.99</span>
-                        </p>
-                        <p>
-                            Status: <span>In Stock</span>
-                        </p>
-                        <p>
-                            Qty
-                            <select>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                            </select>
-                        </p>
-                        <p>
-                            <button type="button">Add To Cart</button>
-                        </p>
-                        </div>
-                </div>
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
+  
+
+  const { id } = useParams(); // Use useParams to get the 'id' parameter
+
+  const productDetails = useSelector((state) => state.getProductDetails);//select state and what want from state
+  const { loading, error, product } = productDetails;//if error or loading show error or loading
+
+  useEffect(() => {
+    if (!product || id !== product._id) {
+      dispatch(getProductDetails(id));
+    }//if product not exist or id not equal to product id then dispatch
+  }, [dispatch, id, product]);
+
+  
+
+  return (
+    <div className="productscreen">
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h2>{error}</h2>//if none of these are true then show product
+      ) : (
+        //fragment to contain multiple elements
+        <>
+          <div className="productscreen__left">
+            <div className="left__image">
+              <img src={product.imageUrl} alt={product.name} />
+            </div>
+            <div className="left__info">
+              <p className="left__name">{product.name}</p>
+              <p>Price: Rs.{product.price}</p>
+              <p>Description: {product.description}</p>
+            </div>
+          </div>
+          <div className="productscreen__right">
+            <div className="right__info">
+              <p>
+                Price:
+                <span>Rs.{product.price}</span>
+              </p>
+              <p>
+                Status:
+                <span>
+                  {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                </span>
+              </p>
+              <p>
+                Qty
+                <select value={qty} onChange={(e) => setQty(e.target.value)}>
+                  {[...Array(product.countInStock).keys()].map((x) => (
+                    <option key={x + 1} value={x + 1}>
+                      {x + 1}
+                    </option>//create a array  of product count in stock and map it to create a option
+                  ))}
+                </select>
+              </p>
+              <p>
+                <button type="button" >
+                  Add To Cart
+                </button>
+              </p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-
-
-    )    
-
+  );
 };
+
 export default ProductScreen;
