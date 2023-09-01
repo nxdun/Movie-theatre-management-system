b7a5d53f965f339ee9cb8d8c6faddd6354e601eb
Omin Hemapriya <it21801372@my.sliit.lk>
2023-09-01 13:29:49 +0530

    New commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const connectDB = require('./config/db');
 
@@ -11,5 +15,7 @@ const app = express();
 
 
 const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
