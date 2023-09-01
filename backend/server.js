2cb498f0bf10eb429336af1b0aa5fa887137eff3
Omin Hemapriya <it21801372@my.sliit.lk>
2023-09-01 15:53:19 +0530

    DB sucssesfully connected

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,27 @@
-//require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-//const connectDB = require('./config/db');
-
-//connectDB();
-
+require('dotenv').config();
 
 const app = express();
 
 
 
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
 
+
 mongoose.connect(URL, {
-    useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true,    
-    useFindAndModify: false
+    useUnifiedTopology: true,   
 });
 
 const connection = mongoose.connection;
