07fb0ec24cfb6deb84ab90f0872799d45423aba8
Omin Hemapriya <it21801372@my.sliit.lk>
2023-09-01 13:52:03 +0530

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,38 @@
-require('dotenv').config();
+//require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-const connectDB = require('./config/db');
+//const connectDB = require('./config/db');
 
-connectDB();
+//connectDB();
 
 
 const app = express();
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8070;
+
 app.use(cors());
 app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,    
+    useFindAndModify: false
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB connection success!");
+});
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
