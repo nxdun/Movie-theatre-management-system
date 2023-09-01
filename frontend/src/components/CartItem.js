77ac5a80f30f469086729ca36227402cd133649f
Vish0111 <it21315282@my.sliit.lk>
2023-09-01 14:17:11 +0530

    render cart item acording to product dynamically

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -1,27 +1,32 @@
 import './CartItem.css';
 import { Link } from 'react-router-dom';
 
+const CartItem = ({item, qtyChangeHandler}) => {
+  return (
+    <div className="cartitem">
+      <div className="cartitem__image">
+        <img 
+        src={item.imageUrl}
+         alt={item.name} 
+         />
+      </div>
 
-const CartItem = () => {
-    return (
-        <div className="cartitem">
-            <div className="cartitem__image">
-            <img src="https://cdn.entertainment-focus.com/wp-content/uploads/2023/05/18224411/fastx_intl_16x9_packshot_publi-1-696x392.jpg" alt="product name" className="cartitem__image"/>
-            </div>
-            <Link to={`/product/${111}`} className="cartitem__name">
-                <p>Product 1</p>
-            </Link>
-            <p className="cartitem__price">$499.99</p>
-            <select className="cartitem__select">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select>
-            <button className="cartitem__deleteBtn">
-                <i className="fas fa-trash"></i>
-            </button>
-            </div>
-    )
-}
+        <Link to={`/product/${item.product}`} className="cartItem__name">
+          <p>{item.name}</p>
+        </Link>
+
+        <p className="cartitem__price">Rs {item.price}</p>
+        
+        <select className="cartitem__select" value={item.qty} onChange={(e)=> qtyChangeHandler(item.product, e.target.value)} >
+          {[...Array(item.countInStock).keys()].map((x) => (//create a array  of product count in stock and map it to create a option
+            <option key={x+1} value={x+1}>{x+1}</option>
+          ))}
+        </select>
+        <button className="cartitem__deleteBtn">
+          <i className="fas fa-trash"></i>
+        </button>
+      </div>
+      );
+};
 
 export default CartItem;
