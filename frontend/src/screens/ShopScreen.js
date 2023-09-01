34ed7b0a1b0ea0e4351370061b7434e890d490c5
Vish0111 <it21315282@my.sliit.lk>
2023-09-01 12:39:30 +0530

    Display products in shop screnn done

--- frontend/src/screens/ShopScreen.js
+++ frontend/src/screens/ShopScreen.js
@@ -1,21 +1,38 @@
 import './ShopScreen.css'
-
+import { useEffect } from 'react'; 
+import { useDispatch, useSelector } from 'react-redux'; 
 //Components
 import Product from '../components/Product';
 
-const Home = () => {
-   
+//Actions
+import { getProducts as listProducts } from '../redux/actions/productActions';//request to backend to get products
+
+const ShopScreen = () => {
+    const dispatch = useDispatch();
+    const getProducts = useSelector((state) => state.getProducts); //Get Products
+    const { products, loading, error } = getProducts;//before rendering loding adn error check
+
+    useEffect(() => {
+        dispatch(listProducts());
+    }, [dispatch]);
+    
+//No loading No Error if none of these are true map through products
+
     return (
         <div className="homescreen">
         <h1 className="homescreen__title">Deals and Exclusives</h1>
         <h2>Get the best deals and exclusives on your favorite movies !!</h2>
         <div className="homescreen__products">
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            </div>
+            {loading ? <h2>loading...</h2> : error ? <h2>{error}</h2> : products.map((product => 
+                <Product key={product._id} productId={product._id}
+                name ={product.name}
+                price={product.price}
+                description={product.description}
+                imageUrl={product.imageUrl}/>)
+                )}
+
+        </div>
         </div>
     );
 };
-export default Home;
+export default ShopScreen;
