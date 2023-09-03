44fa5f6a6d46605fb726d9f9360bb55f2b9cebe4
Vish0111 <it21315282@my.sliit.lk>
2023-09-03 19:40:17 +0530

    functional Searchbar added

--- frontend/src/screens/ShopScreen.js
+++ frontend/src/screens/ShopScreen.js
@@ -1,38 +1,73 @@
 import './ShopScreen.css'
-import { useEffect } from 'react'; 
-import { useDispatch, useSelector } from 'react-redux'; 
-//Components
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts as listProducts } from '../redux/actions/productActions';
 import Product from '../components/Product';
 
-//Actions
-import { getProducts as listProducts } from '../redux/actions/productActions';//request to backend to get products
-
 const ShopScreen = () => {
-    const dispatch = useDispatch();
-    const getProducts = useSelector((state) => state.getProducts); //Get Products
-    const { products, loading, error } = getProducts;//before rendering loding adn error check
+  const dispatch = useDispatch();
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
 
-    useEffect(() => {
-        dispatch(listProducts());
-    }, [dispatch]);
-    
-//No loading No Error if none of these are true map through products
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
 
-    return (
-        <div className="homescreen">
-        <h1 className="homescreen__title">Deals and Exclusives</h1>
-        <h2>Get the best deals and exclusives on your favorite movies !!</h2>
-        <div className="homescreen__products">
-            {loading ? <h2>Loading Products....</h2> : error ? <h2>{error}</h2> : products.map((product => 
-                <Product key={product._id} productId={product._id}
-                name ={product.name}
-                price={product.price}
-                description={product.description}
-                imageUrl={product.imageUrl}/>)
-                )}
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-        </div>
-        </div>
+  useEffect(() => {
+    // Update filteredProducts when products change (initial load)
+    setFilteredProducts(products);
+  }, [products]);
+
+  const searchHandler = (e) => {
+    const keyword = e.target.value.toLowerCase();
+    setSearchInput(keyword);
+
+    // Filter products based on the search input
+    const filtered = products.filter((product) =>
+      product.name.toLowerCase().includes(keyword)
     );
+
+    // Update the filteredProducts state
+    setFilteredProducts(filtered);
+  };
+
+  return (
+    <div className="homescreen">
+      <h1 className="homescreen__title">Deals and Exclusives</h1>
+      <h2>Get the best deals and exclusives on your favorite movies !!</h2>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search Products..."
+          className="search__box"
+          value={searchInput}
+          onChange={searchHandler}
+        />
+      </div>
+
+      <div className="homescreen__products">
+        {loading ? (
+          <h2>loading...</h2>
+        ) : error ? (
+          <h2>{error}</h2>
+        )  : (
+          filteredProducts.map((product) => (
+            <Product
+              key={product._id}
+              productId={product._id}
+              name={product.name}
+              price={product.price}
+              description={product.description}
+              imageUrl={product.imageUrl}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
 };
 export default ShopScreen;
