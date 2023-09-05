30de1b4f165346837f144f95db93bd8f33ffeec9
Vish0111 <it21315282@my.sliit.lk>
2023-09-05 11:10:53 +0530

    adding new products into mongo db by import script

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,40 +1,76 @@
 const products = [
-    {
-      name: "Combo concession Pack 1",
+  {
+    name: "Special family Combo",
+    imageUrl:
+      "https://cinepax.com/IT-CDN/Themes/_default/Images/main6.jpg",
+    description:
+      "2 Large salty pop corn and 4 Large drinks OR 2 Large Caramel pop corn and 4 Large drinks ",
+    price: 1800,
+    countInStock: 10,
+  },
+  {
+    name: "Barbie Pop corn",
+    imageUrl:
+      "https://www.otempo.com.br/image/contentid/policy:1.3016129:1689535818/image.jpeg?f=3x2&q=0.6&w=1200&$p$f$q$w=a356c7d",
+    description:
+      "Special offer for Barbie Fans",
+    price: 500,
+    countInStock: 12,
+  },
+  {
+    name: "Combo concession Pack 1",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYzOTjGhvPUIvzt_ItSRyjLmt_TarDy6W17w&usqp=CAU",
+    description:
+      "Special deal for movie lovers One Large Cheese Popcorn and One Medium Drink.",
+    price: 400,
+    countInStock: 15,
+  },
+  {
+    name: "Combo concession Pack 2",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpUJojmjAeT1T0PczNPd-X0o1cbUg4RHImtQ&usqp=CAU",
+    description:
+      "Special deal for couples One Large Popcorn and Two Drinks (Medium).",
+    price: 900,
+    countInStock: 10,
+  },
+  {
+      name: "Movie Lover Merchandise Pack",
       imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYzOTjGhvPUIvzt_ItSRyjLmt_TarDy6W17w&usqp=CAU",
+        "https://www.fanplus.com/products/842-848.jpg",
       description:
-        "Special deal for movie lovers One Large Cheese Popcorn and One Medium Drink.",
-      price: 400,
-      countInStock: 15,
+        "FAN+ T-Shirt + Cap For Rs.950.",
+      price: 950,
+      countInStock: 20,
     },
     {
-      name: "Combo concession Pack 2",
+      name: "Special BlueBeetle Pack",
       imageUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpUJojmjAeT1T0PczNPd-X0o1cbUg4RHImtQ&usqp=CAU",
+        "https://media.distractify.com/brand-img/xkbl5n_Xn/0x0/cinemark-blue-beetle-1691762984920.jpeg",
       description:
-        "Special deal for couples One Large Popcorn and Two Drinks (Medium).",
-      price: 900,
+        "Special offer for blue beetle Pop corn + Toys ",
+      price: 1500,
+      countInStock: 8,
+    },
+    {
+      name: "Fast X Merchandise Pack",
+      imageUrl:
+        "https://cdn.entertainment-focus.com/wp-content/uploads/2023/05/18224411/fastx_intl_16x9_packshot_publi-1-696x392.jpg",
+      description:
+        "Limited Offer For Fast X, Grab Your Fast X  -T-Shirt(XL,L,M,S Available)-Cap with Free Water Bottel For only Rs:1200.",
+      price: 1200,
       countInStock: 10,
     },
     {
-        name: "Movie Lover Merchandise Pack",
-        imageUrl:
-          "https://www.fanplus.com/products/842-848.jpg",
-        description:
-          "FAN+ T-Shirt + Cap For Rs.950.",
-        price: 950,
-        countInStock: 20,
-      },
-      {
-        name: "Fast X Merchandise Pack",
-        imageUrl:
-          "https://cdn.entertainment-focus.com/wp-content/uploads/2023/05/18224411/fastx_intl_16x9_packshot_publi-1-696x392.jpg",
-        description:
-          "Limited Offer For Fast X, Grab Your Fast X  -T-Shirt(XL,L,M,S Available)-Cap with Free Water Bottel For only Rs:1200.",
-        price: 1200,
-        countInStock: 10,
-      },
+      name: "Kids Combo",
+      imageUrl:
+        "https://i.pinimg.com/originals/58/7d/bb/587dbb04dc3dcb7f25ae77a70362e006.jpg",
+      description:
+        "Special offer for Kids",
+      price: 250,
+      countInStock: 20,
+    },
     
     
   ];
