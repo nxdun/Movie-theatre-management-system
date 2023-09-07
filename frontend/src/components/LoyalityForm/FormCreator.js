9e109536b2a2140387097d114eda3fc38270749f
Nadun Lakshan <inbox.nadun@gmail.com>
2023-09-07 08:51:28 +0530

    in progress of adding validator functionality

--- frontend/src/components/LoyalityForm/FormCreator.js
+++ frontend/src/components/LoyalityForm/FormCreator.js
@@ -1,11 +1,12 @@
 import "./FormCreator.css";
 import Input from "./Input";
+import { VALIDATOR_REQUIRE } from "./LoyalityValidators";
 const FormCreator = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Name" />
+      <Input element="input" type="text" label="Name" errorText = "invalid" validators = {[VALIDATOR_REQUIRE()]}/>
     </form>
   );
-};
+}; 
 
 export default FormCreator;
--- frontend/src/components/LoyalityForm/Input.js
+++ frontend/src/components/LoyalityForm/Input.js
@@ -1,21 +1,23 @@
 import "./Input.css";
 import { useReducer } from "react";
+import { validate } from "./LoyalityValidators";
 
+//input reducer used for handling input change
 const inputReducer = (state, action) => {
   switch (action.type) {
-    case "CHANGE":
+    case 'CHANGE':
       return {
         ...state,
         value: action.val,
-        isValid: true,
+        isValid: validate(action.val, action.validators),
       };
     default:
       return state;
   }
 };
 
+//main input component
 const Input = (props) => {
-
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: "",
     isValid: false,
@@ -23,7 +25,11 @@ const Input = (props) => {
 
   //handling input change connection with useReducer
   const changeHandler = (event) => {
-    dispatch({ type: "CHANGE", val: event.target.value });
+    dispatch({
+      type: 'CHANGE',
+      val: event.target.value,
+      validators: props.validators,
+    });
   };
 
   //this is for adding different input types to form creator as wanted
@@ -44,13 +50,15 @@ const Input = (props) => {
         value={inputState.value}
       />
     );
+    
 
   return (
-    <div className={`formctrl ${!inputState.isValid && 'invalidator'}`}>
+    <div className={`formctrl ${!inputState.isValid && "invalidator"}`}>
       <label htmlFor={props.id}>
         {props.label} :{element}
       </label>
-      {!inputState.isValid && <p>{props.errorText}</p>}
+      <h1>{console.log(inputState.isValid)}</h1>
+      {!inputState.isValid && <h1>{props.errorText}</h1>}
     </div>
   );
   //if isValid is false then show the error text
