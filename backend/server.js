6c2d03d8ac024beafbe1fd2c2b738392d0f45e87
Kalmid <kalmid.98@gmail.com>
2023-09-09 18:28:01 +0530

    Added advertisement controller, router,models and server.js files

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,19 @@ require('dotenv').config();
 const express = require('express');
 
 const connectDB = require('./config/db');
+const mongoose = require("mongoose");
+const Advertisement = require("./model/advertisementModel");
+const advertisementRoutes = require("./routes/advertisementRoute");
 
 connectDB();
 
 
 const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(advertisementRoutes);
 
 
 
