91d7c70d109e3fd90c8fbe912ebbe9cb26edfc67
Omin Hemapriya <it21801372@my.sliit.lk>
2023-09-18 13:47:31 +0530

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,16 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Hi! OMI
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React  
+        </a> */}
       </header>
     </div>
   );
