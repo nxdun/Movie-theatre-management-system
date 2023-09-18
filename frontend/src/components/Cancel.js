62ba9fe5ac34c1abc634855a413d68bf25200ffa
Vish0111 <it21315282@my.sliit.lk>
2023-09-18 22:30:08 +0530

    Success and Cancel pages with css

---
+++ frontend/src/components/Cancel.js
@@ -0,0 +1,12 @@
+import './Cancel.css'
+import React from "react";
+
+
+const Cancel = () => {
+    return (
+        <div class='cancel'>
+            <h1>Payment Cancelled</h1>
+        </div>
+    )
+}
+export default Cancel;
