228fedce848413123a275249f1ff07d145c8f870
Omin Hemapriya <it21801372@my.sliit.lk>
2023-09-20 19:42:24 +0530

    new commit

--- frontend/src/pages/SeatSelect.js
+++ frontend/src/pages/SeatSelect.js
@@ -1,21 +1,46 @@
 import './SeatSelect.css';
-import React, { useState } from 'react';
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import screenImage from './R.png';
 import locationImage from './loca.png';
 import dolbyImage from './dolby.png';
 
 function SeatSelect() {
-  const [selectedButtons, setSelectedButtons] = useState([])
+  const [selectedButtons, setSelectedButtons] = useState([]);
+  const [bookedSeats, setBookedSeats] = useState([]);
+
+  // Define the isSeatBooked function
+  const isSeatBooked = (seatId) => bookedSeats.includes(String(seatId));
+
+  // Fetch booked seat data from the server when the component mounts
+  useEffect(() => {
+    axios.get('http://localhost:5101/booking/')
+      .then((response) => {
+        const bookedSeatIds = response.data.map((booking) => booking.seatId);
+        // Split the comma-separated strings into arrays of seat IDs
+        const allBookedSeats = bookedSeatIds.flatMap(seatIds => seatIds.split(',').map(id => id.trim()));
+        setBookedSeats(allBookedSeats);
+        console.log('Fetched booked seats:', allBookedSeats);
+      })
+      .catch((error) => {
+        console.error('Error fetching booked seats:', error);
+      });
+  }, []);
+
   const handleButtonClick = (seatId) => {
-   
     const newSelectedButtons = [...selectedButtons];
-    
+    const seatIsBooked = isSeatBooked(seatId);
+
+    console.log(`Seat ${seatId} is booked: ${seatIsBooked}`);
+
+    if (seatIsBooked) {
+      // Seat is already booked, don't allow selection
+      return;
+    }
+
     if (newSelectedButtons.includes(seatId)) {
-      
       newSelectedButtons.splice(newSelectedButtons.indexOf(seatId), 1);
     } else {
-      
       newSelectedButtons.push(seatId);
     }
 
@@ -30,20 +55,20 @@ function SeatSelect() {
 
     // Prepare booking data
     const bookingData = {
-      bookingId: 'C1', 
+      bookingId: 'C5',
       bookingDate: new Date(),
       showTime: '6.00PM',
       theaterId: 'A',
       seatId: selectedButtons.join(', '), // Join selected button IDs
       price: selectedButtons.length * 1000, // Calculate price based on the number of selected buttons
-      customerId: 'cus2',
+      customerId: 'cus4',
     };
 
-    // Send a POST request to server to add the booking
+    // Send a POST request to the server to add the booking
     axios.post('http://localhost:5101/booking/add', bookingData)
       .then((response) => {
         console.log(response.data);
-        
+
         setSelectedButtons([]);
         alert('Booking successful!');
       })
@@ -53,46 +78,38 @@ function SeatSelect() {
       });
   };
 
-return (
-  <div>
-    <div className="sticky-div">
-      <h1>AVATAR 2</h1>
-      <h4>GALAXY CINEMA, Colombo</h4>
-      <img src={locationImage} alt="screen" border="0" className="locIcon" />
-      <img src={dolbyImage} alt="screen" border="0" className="dolby" />
-
-    </div>
-
-
+  return (
     <div>
-      <img src={screenImage} alt="screen" border="0"  className="screen-img" />
-    </div>
+      <div className="sticky-div">
+        <h1>AVATAR 2</h1>
+        <h4>GALAXY CINEMA, Colombo</h4>
+        <img src={locationImage} alt="screen" border="0" className="locIcon" />
+        <img src={dolbyImage} alt="screen" border="0" className="dolby" />
+      </div>
 
-    <div className="button-container">
-      {[1, 2, 3, 4, 5].map((seatId) => (
-        <button
-          key={seatId}
-          className={`available ${selectedButtons.includes(seatId) ? 'selected' : ''}`}
-          onClick={() => handleButtonClick(seatId)}
-        >
-          Button {seatId}
-        
-        </button>
-      ))}
-    </div>
+      <div>
+        <img src={screenImage} alt="screen" border="0" className="screen-img" />
+      </div>
 
-    <div>
+      <div className="button-container">
+        {[1, 2, 3, 4, 5].map((seatId) => (
+          <button
+            key={seatId}
+            className={`seat-button ${isSeatBooked(seatId) ? 'unavailable' : (selectedButtons.includes(seatId) ? 'selected' : 'available')}`}
+            onClick={() => handleButtonClick(seatId)}
+          >
+            Button {seatId}
+          </button>
+        ))}
+      </div>
+
+      <div>
         <button className="continue-button" onClick={handleContinue}>
           Continue
         </button>
       </div>
-    
-
-  </div>
- 
-  
-
-)
+    </div>
+  );
 }
 
 export default SeatSelect;
