c8228184a68a20d271e6200a31969144e124fc63
Nadun Lakshan <inbox.nadun@gmail.com>
2023-09-21 08:28:07 +0530

    minor fix

--- frontend/src/components/LoyalityForm/LoyalityRuler.js
+++ frontend/src/components/LoyalityForm/LoyalityRuler.js
@@ -56,7 +56,7 @@ const LoyalityRuler = () => {
     if (Object.keys(errors).length === 0) {
       try {
         console.log(formValues);
-        await axios.patch("/loyalty/", formValues);
+        await axios.patch("/loyality/", formValues);
         setIsSubmit(true);
       } catch (error) {
         console.error("Error submitting data:", error);
