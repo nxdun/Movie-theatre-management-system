c73da3eae27d5b95c4ad8694f90c575459504d52
dunalpasindu <it21389542@my.sliit.lk>
2023-09-21 11:15:51 +0530

    added new data

--- frontend/src/privateDash/screens/addPrivRoom.js
+++ frontend/src/privateDash/screens/addPrivRoom.js
@@ -1,172 +1,126 @@
-import React, { useState } from "react";
+import React, { useCallback, useReducer } from "react";
 
+import Input from "../components/FormElements/Input";
+import Button from "../components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../components/util/validators";
 import "../styles/addPrivRoom.css";
 
-const AddPrivRoom = () => {
-  const emptydata = {
-    screeningRoomName: "",
-    seatCapacity: "",
-    userImage: "",
-    screeningRoomLocation: "",
-    price: "",
-    message: "",
-  };
-
-  const [formData, SetFormData] = useState(emptydata);
-  const [imagePreview, setImagePreview] = useState("");
-
-  const InputChange = (e) => {
-    const { name, value, type } = e.target;
-    if (type === "file") {
-      // Handle file input separately to show the image preview
-      const file = e.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onloadend = () => {
-          setImagePreview(reader.result);
-        };
-        reader.readAsDataURL(file);
-      } else {
-        setImagePreview("");
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputID in state.inputs) {
+        if (inputID === action.inputID) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputID].isValid;
+        }
       }
-    } else {
-      SetFormData((preData) => ({
-        ...preData,
-        [name]: value,
-      }));
-    }
-  };
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputID]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
-  const formSubmitEvent = (e) => {
-    e.preventDefault();
+const AddPrivRoom = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      privscname: {
+        value: "",
+        isValid: false,
+      },
+      privscprice: {
+        value: "",
+        isValid: false,
+      },
+      privseatcapacity: {
+        value: "",
+        isValid: false,
+      },
+      privsclocation: {
+        value: "",
+        isValid: false,
+      },
+      privscdescription: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
 
-    // Reset form on submit
-    e.target.reset();
-    SetFormData(emptydata);
-    setImagePreview("");
-
-    // Result
-    console.log(formData);
-  };
-
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputID: id,
+    });
+  }, []);
   return (
-    <React.Fragment>
-      <div className="row m-0 justify-content-center">
-        <div className="col-md-6 mt-5 mb-5">
-          <div className="form-area">
-            <div className="form-inner">
-              <form onSubmit={formSubmitEvent}>
-                <h4 className="form-heading mb-4 text-primary text-center">
-                  Add Private Screening Room
-                </h4>
-
-                <div className="row">
-
-
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <label className="d-block">Screening Room Name</label>
-                      <input
-                        type="text"
-                        name="screeningRoomName"
-                        className="form-control"
-                        value={formData.screeningRoomName}
-                        onChange={InputChange}
-                      />
-                    </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <label className="d-block">Seat Capacity</label>
-                      <input
-                        type="text"
-                        name="seatCapacity"
-                        className="form-control"
-                        value={formData.seatCapacity}
-                        onChange={InputChange}
-                      />
-                    </div>
-                  </div>
-                 {/* <div className="col-lg-12">
-                    <div className="form-group text-center">
-                    <label className="d-block">Image</label>
-                      <div className="userprofile-img-bx">
-                        <img
-                          id="imgpreviewPrf"
-                          src={
-                            imagePreview || formData.userImage ||
-                            "https://image.flaticon.com/icons/svg/145/145867.svg"
-                          }
-                          alt="profile-img"
-                          className="prf-img-prev"
-                        />
-                        <div className="profile-img-change-btn">
-                          <i className="fa fa-pencil-alt"></i>
-                          <input
-                            type="file"
-                            name="userImage"
-                            className="filepreviewprofile"
-                            onChange={InputChange}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>*/}
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <label className="d-block">Screening Room Location</label>
-                      <input
-                        type="text"
-                        name="screeningRoomLocation"
-                        className="form-control"
-                        value={formData.screeningRoomLocation}
-                        onChange={InputChange}
-                      />
-                    </div>
-                  </div>
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <label className="d-block">Price (Rs.)</label>
-                      <input
-                        type="text"
-                        name="price"
-                        className="form-control"
-                        value={formData.price}
-                        onChange={InputChange}
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-lg-12">
-                    <div className="form-group">
-                      <label className="d-block">Description</label>
-                      <textarea
-                        name="message"
-                        rows="4"
-                        className="form-control"
-                        value={formData.message}
-                        onChange={InputChange}
-                      ></textarea>
-                    </div>
-                  </div>
-                  <div className="col-lg-12 text-right">
-                    <button
-                      type="submit"
-                      className="btn btn-primary form-submit"
-                    >
-                      Add Room
-                    </button>
-                    <button className="btn btn-primary form-cancel">
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
+    <form className="privscreen-form">
+      <h4 className="form-heading mb-4 text-primary text-center">
+        Add Private Screening Room
+      </h4>
+      <Input
+        id="privscname"
+        element="input"
+        type="text"
+        label="Screening Room Name"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid screening name."
+        onInput={inputHandler}
+      />
+      <Input
+        id="privscprice"
+        element="input"
+        type="text"
+        label="Screening Room Price"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid screening price."
+        onInput={inputHandler}
+      />
+      <Input
+        id="privseatcapacity"
+        element="input"
+        type="text"
+        label="Screening Room Seat Capacity"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid screening seat capacity."
+        onInput={inputHandler}
+      />
+      <Input
+        id="privsclocation"
+        element="input"
+        type="text"
+        label="Screening Room Location"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid screening location."
+        onInput={inputHandler}
+      />
+      <Input
+        id="privscdescription"
+        element="textarea"
+        label="Screening Room Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid screening room description.(at least 5 characters.)"
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD ROOM
+      </Button>
+      <Button >Cancel</Button>
+    </form>
   );
 };
 
