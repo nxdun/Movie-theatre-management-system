f5bd215ac9892db97a5d6c831ab22b1401b6b120
Vish0111 <it21315282@my.sliit.lk>
2023-09-24 21:54:16 +0530

    Success page modified

--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -9,6 +9,8 @@ const Success = () => {
             <img className ='success_logo' src="https://craftizen.org/wp-content/uploads/2019/02/successful_payment_388054.png" alt="logo" />
             <h1>Payment Successfull</h1>
             <p>Thank you for your payment.An automated payment recipt will be sent to your registred email.</p>
+            <p>You can show this recipt at the counter to collect your items.</p> 
+            <p>Thank you for choosing our online service!.</p>
         </div>
     )
 }
