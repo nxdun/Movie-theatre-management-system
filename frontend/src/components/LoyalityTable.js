5e8f75975356225e0526be8023668cbbecba5743
Nadun Lakshan <inbox.nadun@gmail.com>
2023-10-14 10:36:20 +0530

    report gen 70% , searck 100%, changed install bat to be error prone

--- frontend/src/components/LoyalityTable.js
+++ frontend/src/components/LoyalityTable.js
@@ -3,6 +3,7 @@ import LoyalitySearchBar from "./LoyalitySearchBar";
 import React, { useState, useEffect } from "react";
 import DataTable, { createTheme } from "react-data-table-component";
 import axios from "axios";
+import easyinvoice from 'easyinvoice';
 
 // createTheme creates a new theme named nadun that overrides the build in dark theme
 createTheme(
@@ -186,8 +187,123 @@ const LoyalityTable = (props) => {
   const newData = data.filter((item) => {
     return item.UserName.toLowerCase().includes(searchTerm.toLowerCase());
   });
-  console.log("st :", searchTerm);
-  console.log(newData);
+
+  var d = {
+    // Customize enables you to provide your own templates
+    // Please review the documentation for instructions and examples
+    "customize": {
+        //  "template": fs.readFileSync('template.html', 'base64') // Must be base64 encoded html 
+    },
+    "images": {
+        // The logo on top of your invoice
+        "logo": "",
+        // The invoice background
+        "background": "https://public.easyinvoice.cloud/img/watermark-draft.jpg"
+    },
+    // Your own data
+    "sender": {
+        "company": "Galaxy cinemas",
+        "address": "the street 123",
+        "zip": "1234 ZIP",
+        "city": "Colombo",
+        "country": "Sri Lanka"
+  
+    },
+    // Your recipient
+    "client": {
+        "company": "Client Corp",
+        "address": "Clientstreet 456",
+        "zip": "4567 CD",
+        "city": "Clientcity",
+        "country": "Clientcountry"
+        // "custom1": "custom value 1",
+        // "custom2": "custom value 2",
+        // "custom3": "custom value 3"
+    },
+    "information": {
+        // Invoice number
+        "number": "2021.0001",
+        // Invoice data
+        "date": "{}",
+        // Invoice due date
+        "due-date": "31-12-2021"
+    },
+    // The products you would like to see on your invoice
+    // Total values are being calculated automatically
+    "products": [
+        {
+            "quantity": 2,
+            "description": "Product 1",
+            "tax-rate": 6,
+            "price": 33.87
+        },
+        {
+            "quantity": 4.1,
+            "description": "Product 2",
+            "tax-rate": 6,
+            "price": 12.34
+        },
+        {
+            "quantity": 4.5678,
+            "description": "Product 3",
+            "tax-rate": 21,
+            "price": 6324.453456
+        }
+    ],
+    // The message you would like to display on the bottom of your invoice
+    "bottom-notice": "this is a auto generated report.",
+    // Settings to customize your invoice
+    "settings": {
+        "currency": "USD", // See documentation 'Locales and Currency' for more info. Leave empty for no currency.
+        // "locale": "nl-NL", // Defaults to en-US, used for number formatting (See documentation 'Locales and Currency')        
+        // "margin-top": 25, // Defaults to '25'
+        // "margin-right": 25, // Defaults to '25'
+        // "margin-left": 25, // Defaults to '25'
+        // "margin-bottom": 25, // Defaults to '25'
+        // "format": "A4", // Defaults to A4, options: A3, A4, A5, Legal, Letter, Tabloid
+        // "height": "1000px", // allowed units: mm, cm, in, px
+        // "width": "500px", // allowed units: mm, cm, in, px
+        // "orientation": "landscape", // portrait or landscape, defaults to portrait
+    },
+    // Translate your invoice to your preferred language
+    "translate": {
+        // "invoice": "FACTUUR",  // Default to 'INVOICE'
+        // "number": "Nummer", // Defaults to 'Number'
+        // "date": "Datum", // Default to 'Date'
+        // "due-date": "Verloopdatum", // Defaults to 'Due Date'
+        // "subtotal": "Subtotaal", // Defaults to 'Subtotal'
+        // "products": "Producten", // Defaults to 'Products'
+        // "quantity": "Aantal", // Default to 'Quantity'
+        // "price": "Prijs", // Defaults to 'Price'
+        // "product-total": "Totaal", // Defaults to 'Total'
+        // "total": "Totaal", // Defaults to 'Total'
+        // "vat": "btw" // Defaults to 'vat'
+    },
+  };
+  
+  //generator
+  const generateInvoice = async () => {
+    try {
+      await easyinvoice.createInvoice(d, function(result) {
+        easyinvoice.print(result.pdf);
+        props.setisprinted(!props.isprinted);
+      });
+    } catch (error) {
+      if (error.message.includes("throttle")) {
+        // Handle throttle exception based on the error message
+        console.error("Throttle Exception: Too many requests. Please try again later.");
+      } else {
+        // Handle other exceptions if needed
+        console.error("An error occurred while generating the invoice:", error);
+      }
+    }
+  };
+  
+  if (props.isprinted) {
+    generateInvoice();
+    props.setisprinted(false);
+  }
+
 
   return (
     <div className="table">
@@ -232,5 +348,7 @@ const LoyalityTable = (props) => {
     </div>
   );
 };
+
+
 
 export default LoyalityTable;
