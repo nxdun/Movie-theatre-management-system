ad1bde3d5665e946e105660daa6b084b8da93080
Nadun Lakshan <inbox.nadun@gmail.com>
2023-10-14 16:47:47 +0530

    fixed some runtime errors

--- install.bat
+++ install.bat
@@ -3,5 +3,5 @@
 
 start cmd /k "npm i"
 start cmd /k "cd frontend & npm install --save react-credit-card-input styled-components easyinvoice --force "
-start cmd /k "cd frontend & npm i "
+start cmd /k "cd frontend & npm i & & npm install --save react-credit-card-input styled-components easyinvoice --force "
 start cmd /k "cd backend & npm i"
