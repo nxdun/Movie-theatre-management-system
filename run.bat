b530770910989e2b0691ce13ab501cb4c50f7601
Nadun Lakshan <inbox.nadun@gmail.com>
2023-10-14 13:07:55 +0530

    hi

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
 @echo off
 
-start cmd /k "npm i"
-start cmd /k "cd frontend & npm install --save react-credit-card-input styled-components easyinvoice --force &  npm i "
-start cmd /k "cd backend & npm i"
+start cmd /k "cd frontend & npm start"
+start cmd /k "cd backend & npm start"
