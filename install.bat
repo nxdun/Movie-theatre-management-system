3679d163ef7834c019efc42e3a5d788861213a8a
Nadun Lakshan <inbox.nadun@gmail.com>
2023-10-14 13:09:47 +0530

    chaneged bat

--- install.bat
+++ install.bat
@@ -2,5 +2,6 @@
 @echo off
 
 start cmd /k "npm i"
-start cmd /k "cd frontend & npm install --save react-credit-card-input styled-components easyinvoice --force &  npm i "
+start cmd /k "cd frontend & npm install --save react-credit-card-input styled-components easyinvoice --force "
+start cmd /k "cd frontend & npm i "
 start cmd /k "cd backend & npm i"
