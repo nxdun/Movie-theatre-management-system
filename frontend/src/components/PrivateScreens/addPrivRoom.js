013c445d76a50ea7893d6931ed2e5e57cb373bdb
dunalpasindu <it21389542@my.sliit.lk>
2023-10-15 22:24:32 +0530

    private sc cart bug fixed

--- frontend/src/components/PrivateScreens/addPrivRoom.js
+++ frontend/src/components/PrivateScreens/addPrivRoom.js
@@ -1,4 +1,4 @@
- import React, { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./inputPrivRoom.css";
